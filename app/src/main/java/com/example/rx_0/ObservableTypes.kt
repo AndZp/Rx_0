72512047c0188d4d8f2dd296249a56700cd3d679
AndZp <andrey.oleynik.zp@gmail.com>
2019-05-27 13:37:12 +0300

    add types

--- app/src/main/java/com/example/rx_0/ObservableTypes.kt
+++ app/src/main/java/com/example/rx_0/ObservableTypes.kt
@@ -1,5 +1,45 @@
 package com.example.rx_0
 
-fun getFlowable(){
+import io.reactivex.*
 
+fun getFlowable():Flowable<String>{
+        return Flowable.create(FlowableOnSubscribe { emmiter ->
+            emmiter.onNext("1")
+            emmiter.onNext("2")
+            emmiter.onNext("3")
+
+            emmiter.onError(IllegalArgumentException())
+            // OR
+            emmiter.onComplete()
+
+        }, BackpressureStrategy.LATEST)
+}
+
+fun getCompletable():Completable{
+    return Completable.create {
+        it.onComplete()
+        //or
+        it.onError(IllegalArgumentException())
+    }
+}
+
+fun getMaybe():Maybe<String>{
+    return Maybe.create {
+        it.onSuccess("Data")
+        // or
+        it.onComplete()
+        //or
+        it.onError(IllegalArgumentException())
+
+    }
+}
+
+
+fun getSingle():Single<String>{
+    return Single.create {
+        it.onSuccess("Data")
+        //or
+        it.onError(IllegalArgumentException())
+
+    }
 }
